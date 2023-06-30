159835d044f159d4fc95f93e2fd2dd29ee00be34
francienevaz <fran.vfraga@gmail.com>
2023-06-30 19:50:34 -0300

    updated

--- Weather_App/index.js
+++ Weather_App/index.js
@@ -1,5 +1,5 @@
 const apiKey = "";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?inits=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
